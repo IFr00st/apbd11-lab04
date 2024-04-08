4dc6f36966ad352590be99edc37aece3bd02accc
s27885 <s27885@pjwstk.edu.pl>
2024-04-08 22:08:19 +0200

    Add some tests

--- zadanie/LegacyApp.tests/UserServiceTests.cs
+++ zadanie/LegacyApp.tests/UserServiceTests.cs
@@ -26,5 +26,67 @@ public class UserServiceTests
         Assert.False(result);
 
     }
+    [Fact]
+    public void AddUser_ReturnsFalseWhenLastNameIsEmpty(){
+    
+        // Arrange
+        var userService = new UserService();
+        // Act
+
+        var result = userService.AddUser(
+            "Jan",
+            null,
+            "kowalski@kowal.com",
+            DateTime.Parse("2000-01-01"),
+            1
+        );
+
+        //Asert
+        
+        Assert.False(result);
+
+    }
+    [Fact]
+    public void AddUser_ReturnsFalseWhenEmailWithoutMonkey(){
+    
+        // Arrange
+        var userService = new UserService();
+        // Act
+
+        var result = userService.AddUser(
+            "Jan",
+            null,
+            "kowalskikowal.com",
+            DateTime.Parse("2000-01-01"),
+            1
+        );
+
+        //Asert
+        
+        Assert.False(result);
+
+    }
+    
+    
+    [Fact]
+    public void AddUser_ReturnsFalseWhenEmailWithoutDot(){
+    
+        // Arrange
+        var userService = new UserService();
+        // Act
+
+        var result = userService.AddUser(
+            "Jan",
+            null,
+            "kowalski@kowalcom",
+            DateTime.Parse("2000-01-01"),
+            1
+        );
+
+        //Asert
+        
+        Assert.False(result);
+
+    }
    
 }
